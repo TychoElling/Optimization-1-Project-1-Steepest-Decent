9fb311664b46f517c12ea19cd402dfcbc858d036
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-02-23 11:05:40 -0800

    Update steep_decent.m

--- steep_decent.m
+++ steep_decent.m
@@ -1,8 +1,8 @@
 function [x2 ] = steep_decent(x0,n)
 %Preforms n iterations
 for bb = 1:n
-    %disp(secant_steep_decent(2,1,x0))
-    x1 = x0 - secant_steep_decent(2,1,x0) * grad_steep_decent(x0(1),x0(2));
+    disp(grad_steep_decent(x0(1),x0(2)))
+    x1 = x0 - secant_steep_decent(0.0000000002,0.0000000001,x0) * grad_steep_decent(x0(1),x0(2));
     x0 = x1;
     %disp(bb);
     %disp(x1);
