0c0b939a3194e70a0dd462d37a651aa787be3a0a
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-02-23 11:06:13 -0800

    Update secant_steep_decent.m

--- secant_steep_decent.m
+++ secant_steep_decent.m
@@ -1,6 +1,5 @@
-function [a3] = secant_steep_decent(a0,a1,x)
+function [a2] = secant_steep_decent(a0,a1,x)
 %Calcluates alphak with secant method
-%Preforms two iterations
 %x0,x1 are initial guesses
 %f is the function being minimized
 %x is the x value
@@ -8,6 +7,15 @@ function [a3] = secant_steep_decent(a0,a1,x)
 %disp(a0);
 
 a2 = a1 - (a1-a0) * phi_steep_decent(a1,x)/(phi_steep_decent(a1,x) -phi_steep_decent(a0,x));
-disp((phi_steep_decent(a1,x) -phi_steep_decent(a0,x)));
-a3 = a2 - (a2-a1) * phi_steep_decent(a2,x)/(phi_steep_decent(a2,x) -phi_steep_decent(a1,x));
+%disp((phi_steep_decent(a1,x) -phi_steep_decent(a0,x)));
+
+for i = 1:1000
+    a2 = a1 - (a1-a0) * phi_steep_decent(a1,x)/(phi_steep_decent(a1,x) -phi_steep_decent(a0,x));
+    a0 = a1;
+    a1 = a2;
+
+
+
+
+
 end
