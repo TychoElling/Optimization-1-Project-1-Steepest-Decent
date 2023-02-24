373950b31c23b4647dee3be4486ddd96c0581128
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-02-23 20:23:25 -0800

    Update phi_steep_decent.m

--- phi_steep_decent.m
+++ phi_steep_decent.m
@@ -1,9 +1,9 @@
 function [y] = phi_steep_decent(alpha,x0)
-%Returns the output of phi function
-%disp(x0);
-b = x0(1);
-%disp(alpha * grad_steep_decent(x0(1),x0(2)));
-a = x0 - alpha * grad_steep_decent(x0(1),x0(2));
-%disp(alpha);
-y = f_steep_decent(a(1),a(2));
+%Returns the output of phi prime function
+y = -1 * grad_steep_decent(x0(1),x0(2));
+y = y';
+a = x0-alpha.*grad_steep_decent(x0(1),x0(2));
+disp(grad_steep_decent(a(1),a(2)))
+disp(y)
+y = y * grad_steep_decent(a(1),a(2));
 end
