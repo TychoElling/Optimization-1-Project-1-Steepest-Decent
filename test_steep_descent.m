69fc6750c4643b6457bbb5c31e35684d58ff916f
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-02-24 07:51:38 -0800

    Update test_steep_descent.m

--- test_steep_descent.m
+++ test_steep_descent.m
@@ -2,4 +2,4 @@ function[n] = test_steep_descent(x0,iter)
 f = @(x) 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
 f_grad = @(x) [400*x(1)^3-400*x(1)*x(2)+2*x(1)-2, 200*(x(2)-x(1)^2)];
 n = steep_descent(x0,f,f_grad,iter);
-end
+end 
