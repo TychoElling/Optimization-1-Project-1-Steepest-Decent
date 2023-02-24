499a2cfa1477396c0335610ca486e9d2e7c48ae3
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-02-24 07:50:32 -0800

    Update secant_steep_descent.m

--- secant_steep_descent.m
+++ secant_steep_descent.m
@@ -3,16 +3,6 @@ fg = f_grad(x0);
 fgg = @(a) f_grad(x0 - a .* fg);
 fggp = @(a) fgg(a)';
 phi_p = @(a) -fg * fggp(a);
-disp(fg)
 %phi_p is the derivative of the phi function
-a0 = 0.01;
-a1 = 0.4;
-disp(fgg(a1))
-for i = 1:2
-    a2 = a1 - (a1 - a0) * phi_p(a1) / (phi_p(a1) -phi_p(a0));
-    %disp(phi_p(a1))
-    %disp(phi_p(a0))
-    a0 = a1;
-    a1 = a2;
-alpha = a2;
+alpha = secant_method(phi_p);
 end
